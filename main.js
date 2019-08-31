5b98a0d919991a3ba68f735f932d8b1b307f1f93
Marc B <marcbaque1311@web.de>
2019-08-31 23:28:43 +0200

    [ UPDATE ] get this year bday from selected bday

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const now = Date.now();
+let now = new Date();
+now.setHours(0, 0, 0, 0);
+now.toLocaleDateString();
+console.log('now', now);
 let nowClear = new Date(now);
 let nowLocalDate = nowClear.toLocaleDateString();
 let westernAge = null;
@@ -10,48 +13,23 @@ const datepicker = document.querySelector('#birthday-picker');
 const ageInput = document.querySelector('.age-input');
 
 ageInput.addEventListener('change', event => {
-    westernAge = event.target.value;    
-})
-
-datepicker.addEventListener('change', event => {  
-  birthdayClear = new Date(event.target.value);
-  birthday = Date.parse(event.target.value);
-  console.log('picked birthday:', birthday);  
-  setCurrentYear(birthdayClear);
-  hasBdayAlreadyPassed();
+  westernAge = event.target.value;
+  westernAge = event.target.value;
+  westernAge = event.target.value;
+  westernAge = event.target.value;
+  westernAge = event.target.value;
 });
 
-function setCurrentYear (bDayClear) {
-    let thisYear = new Date(Date.now()).getFullYear();
-    let formatBday = bDayClear.toLocaleDateString();
-    thisYearBday = formatBday.replace(/\d{4}/g, thisYear);
-    console.log(formatBday, thisYearBday);
+datepicker.addEventListener('change', event => {
+  birthdayClear = new Date(event.target.value);
+  birthdayClear.setHours(0, 0, 0, 0);
+  console.log('picked birthday:', birthdayClear);
+  thisYearDbady = setCurrentYear(birthdayClear);
+  console.log('thisYearDbady', thisYearDbady);
+});
+
+function setCurrentYear(bDayClear) {
+  let thisYear = new Date(Date.now()).getFullYear();
+  let thisYearBdayMs = bDayClear.setFullYear(thisYear);
+  return new Date(thisYearBdayMs);
 }
-
-// check if birthday already pased, wenn true, dann Alter + 1, ansonsten Alter + 2
-function hasBdayAlreadyPassed () {
-    const thisYearBdayMs = Date.parse(Date(thisYearBday));
-    const difference =  thisYearBdayMs - now;
-    console.log('nowLocalDate', nowLocalDate);
-    console.log(thisYearBday == nowLocalDate ? true : false);
-    
-    // if (thisYearBdayMs > now) {
-    //     console.log(thisYearBdayMs,  now,'B-Day not yet passed');
-    // } else if (thisYearBdayMs < now) {
-    //     console.log('B-Day already passed');
-    // }    
-}
-
-
-
-// wenn true, dann Alter + 1, ansonsten Alter + 2
-// function isLastDayOfTheYear(now, thisYear) {
-//   const lastDayOfYear = new Date(`${thisYear}, 12, 31`);
-//   if (now - Date.parse(lastDayOfYear) < 0 ) {
-//       console.log('today not last day of year');
-//       return false;
-//   } else if (now - Date.parse(lastDayOfYear) === 0 ) {
-//     console.log('last day of year');
-//     return true
-//   } 
-// }
