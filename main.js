fee49154d243368f177cbe64ef86e0c06591f9a7
Marc B <marcbaque1311@web.de>
2019-09-01 03:13:14 +0200

    [ UPDATE ] finish module refactor

--- main.js
+++ main.js
@@ -1,21 +1,16 @@
-let now = new Date();
-now.setHours(0, 0, 0, 0);
-let nowClear = new Date(now);
-let nowLocalDate = nowClear.toLocaleDateString();
 let westernAge = null;
 let koreanAge = null;
 let birthdayClear = null;
-let birthday = null;
-let thisYearBday = null;
 
 const datepicker = document.querySelector('#birthday-picker');
 const formControlBtn = document.querySelector('.form-control-btn');
-
 const calculateAgeBtn = document.querySelector('.calculate-age');
 const deleteValuesBtn = document.querySelector('.delete-values');
 const resultContainer = document.querySelector('.result');
 const ageValueWesternElement = document.querySelector('.age-value-western');
 const ageValueKoreanElement = document.querySelector('.age-value-korean');
+
+import { calcKoreanAge } from './korean_age';
 
 if (westernAge == null) {
   formControlBtn.style.display = 'none';
@@ -27,10 +22,7 @@ if (westernAge == null) {
 datepicker.addEventListener('input', event => {
   birthdayClear = new Date(event.target.value);
   birthdayClear.setHours(0, 0, 0, 0);
-  westernAge = getAge(birthdayClear);
-
-  thisYearBday = setCurrentYear(birthdayClear);
-  if (thisYearBday == null || event.target.value === '') {
+  if (birthdayClear == null || event.target.value === '') {
     calculateAgeBtn.setAttribute('disabled', true);
     deleteValuesBtn.setAttribute('disabled', true);
     resultContainer.style.display = 'none';
@@ -41,35 +33,12 @@ datepicker.addEventListener('input', event => {
   }
 });
 
-function getAge(birthday) {
-  let ageDifMs = Date.now() - birthday.getTime();
-  let ageDate = new Date(ageDifMs); // miliseconds from epoch
-  return Math.abs(ageDate.getUTCFullYear() - 1970);
-}
-
-function setCurrentYear(bDayClear) {
-  let thisYear = new Date(Date.now()).getFullYear();
-  let thisYearBdayMs = bDayClear.setFullYear(thisYear);
-  return new Date(thisYearBdayMs);
-}
-
-function hasBirthDayPassed(thisYearBday) {
-  1;
-  return now > thisYearBday;
-}
-
-function getKoreanAge(westernAge, hasBdayPassed) {
-  return hasBdayPassed ? Number(westernAge) + 1 : Number(westernAge) + 2;
-}
-
 calculateAgeBtn.addEventListener('click', e => {
   e.preventDefault();
+  let ages = calcKoreanAge(birthdayClear);
   resultContainer.style.display = 'block';
-  ageValueWesternElement.innerText = westernAge;
-  ageValueKoreanElement.innerText = getKoreanAge(
-    westernAge,
-    hasBirthDayPassed(thisYearBday)
-  );
+  ageValueWesternElement.innerText = ages.westernAge;
+  ageValueKoreanElement.innerText = ages.koreanAge;
 });
 
 deleteValuesBtn.addEventListener('click', e => {
@@ -79,7 +48,6 @@ deleteValuesBtn.addEventListener('click', e => {
   koreanAge = null;
   ageValueWesternElement.innerText = westernAge;
   ageValueKoreanElement.innerText = koreanAge;
-
   formControlBtn.style.display = 'none';
   calculateAgeBtn.setAttribute('disabled', true);
   deleteValuesBtn.setAttribute('disabled', true);
